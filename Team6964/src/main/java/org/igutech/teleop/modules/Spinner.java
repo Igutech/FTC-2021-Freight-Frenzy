c2965324fe6d0208a7628d589778e0fd6cd1edd6
Scratch <Jiamingw29@gmail.com>
2022-01-20 23:11:41 -0500

    reversed power

--- Team6964/src/main/java/org/igutech/teleop/modules/Spinner.java
+++ Team6964/src/main/java/org/igutech/teleop/modules/Spinner.java
@@ -7,7 +7,7 @@ import org.igutech.teleop.Teleop;
 @Config
 public class Spinner extends Module {
     private GamepadService gamepadService;
-    public static double maxPow = -0.65;
+    public static double maxPow = 0.65;
     public Spinner() {
         super(200, "Spinner");
     }
@@ -23,7 +23,7 @@ public class Spinner extends Module {
         if(gamepadService.getDigital(2,"left_bumper")){
             Teleop.getInstance().getHardware().getMotors().get("spinner").setPower(maxPow);
         }else if(gamepadService.getDigital(2,"right_bumper")){
-            Teleop.getInstance().getHardware().getMotors().get("spinner").setPower(-1);
+            Teleop.getInstance().getHardware().getMotors().get("spinner").setPower(1);
         }else{
             Teleop.getInstance().getHardware().getMotors().get("spinner").setPower(0);
         }
